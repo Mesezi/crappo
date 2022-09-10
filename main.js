4ba77725e7fba4ea4e430eed4d8a530f5ee6701b
Menuchim <msezi.usv@gmail.com>
2022-09-10 15:24:56 +0100

    refactoring

--- main.js
+++ main.js
@@ -1,40 +1,15 @@
 let exchangeText = document.getElementById('exchange')
-let countriesText = document.getElementById('countries')
+let countriesText = document.getElementById('countries') 
 let usersText = document.getElementById('users')
 
 let exchange = 20
 let countries = 185
 let users = 0
-
 let interval = 0
 
-// Function to run at irregular intervals
-function incrementCount() {
-    if(interval < 1){
-        interval = setInterval(() => {
-            if(users < 10){
-                users++
-                usersText.innerText = users
-            }
+let cryptoCon = document.querySelectorAll(".crypto-con"); // all crypto elements
 
-            if(exchange < 30){
-                exchange++
-                exchangeText.innerText = exchange
-            } 
-
-            if(countries < 195){
-                countries++
-                countriesText.innerText = countries
-            } 
-        
-        }, 200);  
-    }
-} 
-
-
-let cryptoCon = document.querySelectorAll(".crypto-con");
-
-cryptoCon.forEach((crypto, index) => {
+cryptoCon.forEach((crypto) => { // loop through each crypto element 
     crypto.addEventListener('click', () => {
         cryptoCon.forEach(elem =>{
             elem.classList.remove('current') // remove current class from all crypto containers
@@ -45,7 +20,9 @@ cryptoCon.forEach((crypto, index) => {
 
 
 
-let left = document.querySelectorAll(".from-left");
+
+// elements that will need animation 
+let left = document.querySelectorAll(".from-left"); 
 let right = document.querySelectorAll(".from-right");
 let scale = document.querySelectorAll(".scale-small");
 let bottom = document.querySelectorAll(".from-bottom");
@@ -53,11 +30,30 @@ let icons = document.querySelectorAll(".from-left-icons");
 
 
 function counter(){
-    let distInView = icons[1].getBoundingClientRect().top - window.innerHeight + 80;
-    if (distInView >= -1000 && distInView <= 0){
-        incrementCount();
+    let inView = icons[1].getBoundingClientRect().top - window.innerHeight;
+    if (inView >= -1000 && inView <= 0){ // when element is within this view range
+        if(interval < 1){//run this code only when interval is NOT active
+            interval = setInterval(() => {
+                if(users < 10){
+                    users++
+                    usersText.innerText = users
+                }
+    
+                if(exchange < 30){
+                    exchange++
+                    exchangeText.innerText = exchange
+                } 
+    
+                if(countries < 195){
+                    countries++
+                    countriesText.innerText = countries
+                } 
+            
+            }, 200);  
+        }
 }
 else{
+    // if element is not within range reset to default values
     exchange = 20 
     countries = 185
     users = 0  
@@ -66,12 +62,13 @@ else{
 
 
 
-window.addEventListener('scroll', animation); 
+window.addEventListener('scroll', animation); // on every scroll run the animation function
+
 function animation() {
-    for (let i = 0; i < bottom.length; i++) {
-        let elem = bottom[i]
-        let distInView = elem.getBoundingClientRect().top - window.innerHeight + 80;
-        if (distInView >= -1000 && distInView <= 0) {
+    for (let i = 0; i < bottom.length; i++) { 
+        let elem = bottom[i] // element that requires bottom animation
+        let inView = elem.getBoundingClientRect().top - window.innerHeight + 100;
+        if (inView >= -1000 && inView <= 0) {// when element is within this view range
            elem.classList.add('slideBottom')
         } else {
             elem.classList.remove('slideBottom')
@@ -79,9 +76,9 @@ function animation() {
     }
 
     for (let i = 0; i < left.length; i++) {
-        let elem = left[i]
-        let distInView = elem.getBoundingClientRect().top - window.innerHeight + 80;
-        if  (distInView >= -1000 && distInView <= 0) {
+        let elem = left[i] // element that requires left animation
+        let inView = elem.getBoundingClientRect().top - window.innerHeight + 100;
+        if  (inView >= -1000 && inView <= 0) {// when element is within this view range
            elem.classList.add('slideLeft')
         } else {
             elem.classList.remove('slideLeft')
@@ -89,9 +86,9 @@ function animation() {
     }
 
     for (let i = 0; i < right.length; i++) {
-        let elem = right[i]
-        let distInView = elem.getBoundingClientRect().top - window.innerHeight + 80;
-        if  (distInView >= -1000 && distInView <= 0) {
+        let elem = right[i] // element that requires right animation
+        let inView = elem.getBoundingClientRect().top - window.innerHeight + 100;
+        if  (inView >= -1000 && inView <= 0) {// when element is within this view range
            elem.classList.add('slideRight')
         } else {
             elem.classList.remove('slideRight')
@@ -99,9 +96,9 @@ function animation() {
     }
 
     for (let i = 0; i < scale.length; i++) {
-        let elem = scale[i]
-        let distInView = elem.getBoundingClientRect().top - window.innerHeight + 80;
-        if (distInView >= -1000 && distInView <= 0) {
+        let elem = scale[i] // element that requires scale animation
+        let inView = elem.getBoundingClientRect().top - window.innerHeight + 100;
+        if (inView >= -1000 && inView <= 0) {// when element is within this view range
            elem.classList.add('scale')
         } else {
             elem.classList.remove('scale')
@@ -109,22 +106,21 @@ function animation() {
     }
 
     for (let i = 0; i < icons.length; i++) {
-        let elem = icons[i]
-        let distInView = elem.getBoundingClientRect().top - window.innerHeight + 80
-        if (distInView >= -1000 && distInView <= 0) {
+        let elem = icons[i] // element that requires left animation
+        let inView = elem.getBoundingClientRect().top - window.innerHeight + 100
+        if (inView >= -1000 && inView <= 0) {// when element is within this view range
            elem.classList.add('slideLeft')
         } else {
             elem.classList.remove('slideLeft')
         }
     }
 
-    
-counter();
+counter(); // run counter function
 }
 
 animation();
 
-
+// Open and Close Menu
 let openMenu = document.getElementById('openMenu')
 let closeMenu = document.getElementById('closeMenu')
 let menuBox = document.querySelector('.menu')
